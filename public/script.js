9e8dbd8a7cc2c1bb2819b6f56be94b8000367dff
laigOSU <35331156+laigOSU@users.noreply.github.com>
2018-03-13 20:21:02 -0700

    Client consoles JSON data. Now display to user.

--- public/script.js
+++ public/script.js
@@ -6,22 +6,17 @@
 //https://stackoverflow.com/questions/18831783/how-to-call-a-server-side-function-from-client-side-e-g-html-button-onclick-i
 var addButtonFunction = function(){
   var button = document.createElement('button');
-  // button.id="lumosButton"
   button.id="addButton"
   button.innerHTML = 'ADD WORKOUT';
   button.onclick = function(){
-    // alert('here be dragons');
     return false;
   };
-  // where do we want to have the button to appear?
-  // you can append it to another element just by doing something like
-  // document.getElementById('foobutton').appendChild(button);
 
   document.getElementById('addWorkoutForm').appendChild(button);
     // document.body.appendChild(button);
 };
 
-//CALL THIS F'N FOR LUMOSBUTTON TO SHOW
+//CALL THIS F'N FOR ADD WORKOUT BUTTON TO SHOW
 addButtonFunction();
 
 
@@ -30,21 +25,21 @@ addButtonFunction();
 1. DISPLAY  (new xml, GET)
 *****************************************************************************/
 //Get form id
-var addStudent = document.getElementById("addWorkoutForm");
+var addWorkout = document.getElementById("addWorkoutForm");
 
 //Create a request in order to add the element to the server
 var ourRequest = new XMLHttpRequest();
 
-//GET request to /display
-ourRequest.open('GET', '/display', true); //was home1
+//GET request to /display route
+ourRequest.open('GET', '/display', true);
 
 //Do something with the data when it gets loaded.
 //Alternatively can do: ourRequest.addEventListener("load", function(){});
-ourRequest.onload = function(){// ourRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+ourRequest.onload = function(){ // ourRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   //A. If the data from server loads properly, then do something with it.
   if(ourRequest.status >= 200 && ourRequest.status < 400){
     var ourData = JSON.parse(ourRequest.responseText); //Parse the loaded JSON data
-    console.log(ourData); //Display the objects to console
+    console.log(ourData); //Display the objects to client console
 
     //Dynamically create a table to display the loaded ourData (f'n definition below)
     generate_table(ourData);
@@ -74,27 +69,39 @@ ourRequest.send();
       <td>  <td>  <td>
 */
 function generate_table(loadedJSON){
-  //Get the table (from home0)
+  //Get the table (from home.handlebars)
   var table = document.getElementById("displayTable");
 
   //Create a header row <tr> with header cells <th>
   var head_row = document.createElement("tr");
 
-  var head_fname = document.createElement("th");
-  var head_lname = document.createElement("th");
-  var head_house = document.createElement("th");
+  var head_name = document.createElement("th");
+  var head_reps = document.createElement("th");
+  var head_weight = document.createElement("th");
+  var head_date = document.createElement("th");
+  var head_unit = document.createElement("th");
   var head_update = document.createElement("th");
   var head_delete = document.createElement("th");
 
   //Append upward to each parent
-  head_fname.innerText = "First Name";
-  head_row.appendChild(head_fname);
-  head_lname.innerText = "Last Name";
-  head_row.appendChild(head_lname);
-  head_house.innerText = "House";
-  head_row.appendChild(head_house);
+  head_name.innerText = "Exercise";
+  head_row.appendChild(head_name);
+
+  head_reps.innerText = "Reps";
+  head_row.appendChild(head_reps);
+
+  head_weight.innerText = "Weight";
+  head_row.appendChild(head_weight);
+
+  head_date.innerText = "Date";
+  head_row.appendChild(head_date);
+
+  head_unit.innerText = "Unit";
+  head_row.appendChild(head_unit);
+
   head_update.innerText = "Update";
   head_row.appendChild(head_update);
+
   head_delete.innerText = "Delete";
   head_row.appendChild(head_delete);
 
@@ -107,10 +114,11 @@ function generate_table(loadedJSON){
     //Create new row/cells to put the loaded JSON in
     var add_row = document.createElement("tr");
 
-    var add_fname = document.createElement("td");
-    var add_lname = document.createElement("td");
-    var add_house = document.createElement("td");
-    var add_update = document.createElement("td");
+    var add_name = document.createElement("td");
+    var add_reps = document.createElement("td");
+    var add_weight = document.createElement("td");
+    var add_date = document.createElement("td");
+    var add_unit = document.createElement("td");
     var add_delete = document.createElement("td");
 
     /*loaded JSON on client console:
@@ -120,18 +128,18 @@ function generate_table(loadedJSON){
       //https://www.w3schools.com/jsref/jsref_substring.asp
 
     // if (item["fname"] != null){
-      add_fname.innerText = item["fname"].substring(0);   //}  curly from if statemetn
-      add_row.appendChild(add_fname);
-
-      add_lname.innerText = item["lname"].substring(0);
-      add_row.appendChild(add_lname);
-
-      //becuase item["house"] is not a substring
-      add_house.innerText = item["house"];
-      add_row.appendChild(add_house);
-
-      //NEED TO APPEND!!
-      table.appendChild(add_row);
+      // add_fname.innerText = item["fname"].substring(0);   //}  curly from if statemetn
+      // add_row.appendChild(add_fname);
+      //
+      // add_lname.innerText = item["lname"].substring(0);
+      // add_row.appendChild(add_lname);
+      //
+      // //becuase item["house"] is not a substring
+      // add_house.innerText = item["house"];
+      // add_row.appendChild(add_house);
+      //
+      // //NEED TO APPEND!!
+      // table.appendChild(add_row);
   })
 }
 
@@ -139,61 +147,61 @@ function generate_table(loadedJSON){
 /*****************************************************************************
 2. INSERT
 *****************************************************************************/
-var addButton = document.getElementById("addButton");
-
-addButton.addEventListener('click', function(event){  //I don't think I need an event parameter. Anyway.
-  console.log("I'm going to work on sending these post parameters to server");
-
-  var addReq = new XMLHttpRequest();
-
-  //https://www.w3schools.com/js/js_json_arrays.asp
-  // {id: 2, fname: "Harry", lname: "Potter", house: 9}
-  //Create the payload object and set all elements to null
-  var payload = {fname: null,
-                lname: null,
-                house: null};
-
-    console.log(typeof fname);
-    console.log(typeof lname);
-    console.log(typeof house);
-    console.log("HP client going to try sending to HP server")
-
-  //Now set the object elements to client input (addWorkoutForm form parameters)
-  //OR leave it null, whichever is true
-  //and then reset the previous value in the form
-  payload.fname = document.getElementById("fname").value || null;
-   document.getElementById("fname").value = null;
-
-  payload.lname = document.getElementById("lname").value || null;
-  document.getElementById("lname").value = null;
-
-  payload.house = document.getElementById("house").value || null;
-  document.getElementById("house").value = null;
-
-
-  //I might need to setRequestHeader
-  addReq.open('POST', '/insert', true);
-  addReq.setRequestHeader('Content-Type', 'application/json');
-
-  addReq.onload = function(){
-     //A. If the data from server loads properly, then do something with it.
-     if(ourRequest.status >= 200 && ourRequest.status < 400){
-       var addedData = JSON.parse(ourRequest.responseText);
-       //Dynamically create a table to display the loaded addedData
-       generate_table(addedData);
-     }
-    //B. If data fails to load from the server, print error message.
-     else {
-       console.log("Error in network request: " + ourRequest.statusText);
-     }
-     // addReq.send(JSON.stringify(payload));
-  }
-
-  //req.send() here
-  //addReq.send(JSON.stringify(payload));
-  addReq.send(payload);
-  event.preventDefault();
-});
+// var addButton = document.getElementById("addButton");
+//
+// addButton.addEventListener('click', function(event){  //I don't think I need an event parameter. Anyway.
+//   console.log("I'm going to work on sending these post parameters to server");
+//
+//   var addReq = new XMLHttpRequest();
+//
+//   //https://www.w3schools.com/js/js_json_arrays.asp
+//   // {id: 2, fname: "Harry", lname: "Potter", house: 9}
+//   //Create the payload object and set all elements to null
+//   var payload = {fname: null,
+//                 lname: null,
+//                 house: null};
+//
+//     console.log(typeof fname);
+//     console.log(typeof lname);
+//     console.log(typeof house);
+//     console.log("HP client going to try sending to HP server")
+//
+//   //Now set the object elements to client input (addWorkoutForm form parameters)
+//   //OR leave it null, whichever is true
+//   //and then reset the previous value in the form
+//   payload.fname = document.getElementById("fname").value || null;
+//    document.getElementById("fname").value = null;
+//
+//   payload.lname = document.getElementById("lname").value || null;
+//   document.getElementById("lname").value = null;
+//
+//   payload.house = document.getElementById("house").value || null;
+//   document.getElementById("house").value = null;
+//
+//
+//   //I might need to setRequestHeader
+//   addReq.open('POST', '/insert', true);
+//   addReq.setRequestHeader('Content-Type', 'application/json');
+//
+//   addReq.onload = function(){
+//      //A. If the data from server loads properly, then do something with it.
+//      if(ourRequest.status >= 200 && ourRequest.status < 400){
+//        var addedData = JSON.parse(ourRequest.responseText);
+//        //Dynamically create a table to display the loaded addedData
+//        generate_table(addedData);
+//      }
+//     //B. If data fails to load from the server, print error message.
+//      else {
+//        console.log("Error in network request: " + ourRequest.statusText);
+//      }
+//      // addReq.send(JSON.stringify(payload));
+//   }
+//
+//   //req.send() here
+//   //addReq.send(JSON.stringify(payload));
+//   addReq.send(payload);
+//   event.preventDefault();
+// });
 
 /*****************************************************************************
 3. DELETE
